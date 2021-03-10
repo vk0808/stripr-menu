6ed741abd6edd40209cb9043158242665f11ff44
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-03-10 19:57:37 +0530

    hero component with style

--- src/Hero.js
+++ src/Hero.js
@@ -1,13 +1,26 @@
-import phoneImg from './images/phone.svg';
-import { useGlobalContext } from './context';
+import phoneImg from "./images/phone.svg";
+import { useGlobalContext } from "./context";
 
 const Hero = () => {
-  const data = useGlobalContext();
-  
+  const { closeSubmenu } = useGlobalContext();
+
   return (
-    <>
-      <p>Hero</p>
-    </>
-  )
-}
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments Infrastruture for the Internet</h1>
+          <p>
+            Millions of companies of all sizes—from startups to Fortune 500s—use
+            Stripe’s software and APIs to accept payments, send payouts, and
+            manage their businesses online.
+          </p>
+          <button className="btn">start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="phone" />
+        </article>
+      </div>
+    </section>
+  );
+};
 export default Hero;
