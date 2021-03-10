8dc20b7122c91206ce3a34a7cdb8a3413346c0d5
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-03-10 23:53:29 +0530

    added submenu links

--- src/context.js
+++ src/context.js
@@ -7,6 +7,7 @@ export const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
   const [location, setLocation] = useState({});
+  const [page, setPage] = useState({page: '', links: []});
 
   const openSidebar = () => {
     setIsSidebarOpen(true);
@@ -15,6 +16,8 @@ export const AppProvider = ({ children }) => {
     setIsSidebarOpen(false);
   };
   const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
     setLocation(coordinates);
     setIsSubmenuOpen(true);
   };
@@ -31,7 +34,8 @@ export const AppProvider = ({ children }) => {
         closeSidebar,
         openSubmenu,
         closeSubmenu,
-        location
+        location,
+        page
       }}
     >
       {children}
